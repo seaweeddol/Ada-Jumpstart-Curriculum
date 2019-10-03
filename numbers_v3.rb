804f15b52f0d3e110f5cc8631f5c1d03c6fd196f
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-03 13:45:43 -0700

    add range loop

--- numbers_v3.rb
+++ numbers_v3.rb
@@ -4,7 +4,7 @@ How many times you would like to run this program? "
 count = gets.chomp.to_i
 
 =begin
-# While loop that asks for three numbers and prints out if they are divisible by 3 or not.
+# While loop that asks for numbers and prints out if they are divisible by 3 or not.
 
 i = 0
 
@@ -18,10 +18,21 @@ while i < count do
   end
   i += 1
 end
+
+# Counter-controlled loop that asks for numbers and prints out if they are divisible by 3 or not.
+count.times do
+  print "Enter a positive integer value: "
+  num = gets.chomp.to_i
+  if num % 3 == 0
+    puts "#{num} is divisible by 3."
+  else
+    puts "#{num} is not divisible by 3."
+  end
+end
 =end
 
-# While loop that asks for three numbers and prints out if they are divisible by 3 or not.
-count.times do
+# Range loop that asks for numbers and prints out if they are divisible by 3 or not.
+(1..count).each do |i|
   print "Enter a positive integer value: "
   num = gets.chomp.to_i
   if num % 3 == 0
