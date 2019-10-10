030f47c393a755e2b5a87b86d91cf2b0aeb811a7
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-10 11:28:43 -0700

    check for password length and add password feedback

--- password_verification.rb
+++ password_verification.rb
@@ -1,14 +1,24 @@
- password_valid = false
+password_valid = false
 
+# loop until password is valid
 until password_valid
   print "Enter a new password: "
   password = gets.chomp
-  if password =~ /\d/
-    if password =~ /@|%|\*|!/
-      puts "good job!"
-      password_valid = true
+  # check if password is at least 8 characters long
+  if password.length >= 8
+    # check if password includes a number
+    if password =~ /\d/
+      # check if password includes a special character
+      if password =~ /@|%|\*|!/
+        puts "good job!"
+        password_valid = true
+      else
+        puts "Password must contain a special character (@, %, *, or !)"
+      end
+    else
+      puts "Password must contain a number."
     end
   else
-    puts "no"
+    puts "Password must be at least 8 characters."
   end
 end
