4f5d6976b6de4dfdbd7793d59e5dcdaac4989baf
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-10 12:59:50 -0700

    start error message logic

--- password_verification.rb
+++ password_verification.rb
@@ -1,30 +1,31 @@
 password_valid = false
+password_error = []
 
 # loop until password is valid
 until password_valid
   print "Enter a new password: "
   password = gets.chomp
-  # check if password is at least 8 characters long
-  if password.length >= 8
-    # check if password includes a number
-    if password =~ /\d/
-      # check if password includes a special character
-      if password =~ /@|%|\*|!/
-        print "Please enter your password again to confirm: "
-        password_verify = gets.chomp
-        until password_verify == password
-          print "Your password did not match. Please enter it again: "
-          password_verify = gets.chomp
-        end
-        puts "Your password has been accepted."
-        password_valid = true
-      else
-        puts "Password must contain a special character (@, %, *, or !)"
-      end
-    else
-      puts "Password must contain a number."
+  if password.length >= 8 && password =~ /\d/ && password =~ /@|%|\*|!/
+    print "Please enter your password again to confirm: "
+    password_verify = gets.chomp
+    until password_verify == password
+      print "Your password did not match. Please enter it again: "
+      password_verify = gets.chomp
     end
+    puts "Your password has been accepted."
+    password_valid = true
+  # check if password is at least 8 characters long
   else
-    puts "Password must be at least 8 characters."
+    if password.length < 8
+      password_error.push("be at least 8 characters long")
+    # check if password includes a number
+    elsif password !=~ /\d/
+      password_error.push("contain a number")
+    # check if password includes a special character
+    elsif password !=~ /@|%|\*|!/
+      password_error.push("contain a special character (@, %, *, or !)")
+    end
+    print password_error
+    password_error = []
   end
 end
