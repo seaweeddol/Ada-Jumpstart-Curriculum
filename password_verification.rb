74baa895c845d4415f4af2934a8f233716b800b3
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-10 11:21:11 -0700

    Create password_verification.rb

---
+++ password_verification.rb
@@ -0,0 +1,14 @@
+ password_valid = false
+
+until password_valid
+  print "Enter a new password: "
+  password = gets.chomp
+  if password =~ /\d/
+    if password =~ /@ + % + \* + !/
+      puts "good job!"
+      password_valid = true
+    end
+  else
+    puts "no"
+  end
+end
