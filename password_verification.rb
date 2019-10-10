440179bf1e3a83ecd555b2d429b040fcaad1e04d
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-10 13:14:01 -0700

    add check for lowercase and uppercase letter

--- password_verification.rb
+++ password_verification.rb
@@ -5,8 +5,8 @@ password_error = []
 until password_valid
   print "Enter a new password: "
   password = gets.chomp
-  if password.length >= 8 && password =~ /\d/ && password =~ /@|%|\*|!/
-    print "Please enter your password again to confirm: "
+  if password.length >= 8 && password =~ /\d/ && password =~ /@|%|\*|!/ && password =~ /[a-z]/ && password =~ /[A-Z]/
+    print "Enter again to confirm: "
     password_verify = gets.chomp
     until password_verify == password
       print "Your password did not match. Please enter it again: "
@@ -26,6 +26,14 @@ until password_valid
     # check if password includes a special character
     if password !~ /@|%|\*|!/
       password_error.push("contain a special character (@, %, *, or !)")
+    end
+    # check if password includes a lowercase letter
+    if password !~ /[a-z]/
+      password_error.push("contain a lowercase letter")
+    end
+    # check if password includes an uppercase letter
+    if password !~ /[A-Z]/
+      password_error.push("contain an uppercase letter")
     end
     puts "Password must #{password_error * ", "}"
     password_error = []
