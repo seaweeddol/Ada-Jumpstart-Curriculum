024fe71e6913f6201ffbce62f3d490ba15ba1b15
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-10 15:05:29 -0700

    Update numbers_v4.rb

--- numbers_v4.rb
+++ numbers_v4.rb
@@ -2,6 +2,7 @@ print "How many numbers would you like to work with? "
 number_count = gets.chomp.to_i
 numbers = [0]
 
+# determine if current value is greater, less, or equal to previous value
 number_count.times do |i|
   print "Enter a positive integer: "
   numbers.push(gets.chomp.to_i)
@@ -15,3 +16,23 @@ number_count.times do |i|
 end
 
 numbers.shift
+min = numbers[0]
+total = 0
+
+# loop to find lowest value in the numbers array
+numbers.each do |num|
+  if num < min
+    min = num
+  end
+end
+
+# loop to find average of numbers array
+numbers.each do |num|
+  total += num
+end
+
+avg = (total.to_f / numbers.length).to_f
+
+print "The minimum value in the array is #{min}
+The maximum value in the array is #{numbers.max}
+The average of the array is #{avg}"
