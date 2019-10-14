2415cd3d01ba8c48b03dc707bd0a121707d33f11
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-14 12:26:41 -0700

    add loop to create email address

--- student_account_generator.rb
+++ student_account_generator.rb
@@ -10,3 +10,8 @@ end
 5.times do |i|
   student_ID[i] = rand(111111..999999)
 end
+
+5.times do |i|
+  student_email[i] = student_name[i][0] + student_name[i].split.last + student_ID[i][4].to_s + student_ID[i][5].to_s + student_ID[i][6].to_s + "@adadevelopersacademy.org"
+  puts student_email[i]
+end
