9a825e9396894df50bd61b4a237a6aec270424f2
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-14 13:17:29 -0700

    add logic to prevent duplicate student_ID generation

--- student_account_generator.rb
+++ student_account_generator.rb
@@ -1,17 +1,24 @@
 student_name = []
 student_ID = []
 student_email = []
+random_ID = rand(111111..999999).to_s
 
+# get first and last name
 5.times do |i|
   print "Enter your first and last name: "
   student_name[i] = gets.chomp.upcase
 end
 
+# generate random ID
 5.times do |i|
-  student_ID[i] = rand(111111..999999).to_s
+  while student_ID.include?(random_ID) == true
+    random_ID = rand(111111..999999).to_s
+  end
+  student_ID[i] = random_ID
   puts student_ID[i]
 end
 
+# generate email address
 5.times do |i|
   student_email[i] = student_name[i][0] + student_name[i].split.last + student_ID[i][3,3] + "@adadevelopersacademy.org"
   puts student_email[i]
