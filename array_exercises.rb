6d63d8e62dfcd3047739b064cabf912035a319c0
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-15 12:48:33 -0700

    complete second problem

--- array_exercises.rb
+++ array_exercises.rb
@@ -1,9 +1,30 @@
 # Create an array to store 5 names. Have the user enter in the 5 names and then print out the 5 names in all UPPERCASE all on the same line. Note that the user may not enter all the names in uppercase.
 
-names = []
-5.times do |i|
-  print "Enter a name: "
-  names[i] = gets.chomp.upcase
+# names = []
+# 5.times do |i|
+#   print "Enter a name: "
+#   names[i] = gets.chomp.upcase
+# end
+#
+# puts names.join(' ')
+
+# Create an array to store 3 boolean values entered in by the user. Print out YES if all the values are true and NO if at least one value is false.
+
+boolean = []
+boolean_print = "YES"
+
+3.times do |i|
+  print "Enter true or false: "
+  boolean[i] = gets.chomp.downcase
+  if boolean[i] == "true"
+    boolean[i] = true
+  else
+    boolean[i] = false
+  end
 end
 
-print names
+if boolean.include?(false)
+  boolean_print = "NO"
+end
+
+puts boolean_print
