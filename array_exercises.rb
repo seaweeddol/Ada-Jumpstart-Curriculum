c492fc5bdafc7b16c95c159c193435db46090de3
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-16 13:01:59 -0700

    update 3rd problem to remove "fire" from words without altering the array

--- array_exercises.rb
+++ array_exercises.rb
@@ -31,14 +31,11 @@
 
 # 3. Create an array to store the following words: firetruck, fire drill, fire hydrant, firefighter, fireproof, fire station, fire hose. Then write code that uses the array to print out truck, drill, hydrant, fighter, proof, station, hose without modifying the array.
 
-# fire_array = ["firetruck", "fire drill", "fire hydrant", "firefighter", "fireproof", "fire station", "fire hose"]
-#
-# fire_array.length.times do |i|
-#   puts fire_array[i].slice!("fire")
-#   puts fire_array[i]
-# end
-#
-# puts fire_array
+fire_array = ["firetruck", "fire drill", "fire hydrant", "firefighter", "fireproof", "fire station", "fire hose"]
+
+fire_array.length.times do |i|
+  puts fire_array[i].gsub("fire", "").strip
+end
 
 # 4. Create an array to store the amount of money spent on 4 people during the Holidays. Have the user enter in the amount spent on each person. Print the total spent on all the people. Total money spent should be displayed with a dollar sign, decimal point, and only 2 digits after the decimal.
 
