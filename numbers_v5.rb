7d13b58ba44d7859a87047007eb00e48281c8bb3
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-17 10:11:15 -0700

    add comments and update variable names

--- numbers_v5.rb
+++ numbers_v5.rb
@@ -1,61 +1,73 @@
-hash = {
+# create first hash
+first_hash = {
   random_numbers: [],
   user_input: []
 }
 
+# add 5 random numbers to the first array in first_hash
 5.times do |i|
-  hash[:random_numbers].push(rand(12..21))
+  first_hash[:random_numbers].push(rand(12..21))
 end
 
+# add 5 numbers provided by the user to the second array in first_hash
 5.times do |i|
   print "Enter a number between 12 and 21: "
-  hash[:user_input].push(gets.chomp.to_i)
+  first_hash[:user_input].push(gets.chomp.to_i)
 end
 
-hash2 = {
+# create second hash
+second_hash = {
   computer_generated: {},
   user_generated: {}
 }
 
-hash[:random_numbers].each do |i|
-  if hash2[:computer_generated].key?(i)
-    hash2[:computer_generated][i] += 1
+# fill second_hash with random_numbers values from first_hash
+first_hash[:random_numbers].each do |i|
+  # if key already exists, add 1 to the value
+  if second_hash[:computer_generated].key?(i)
+    second_hash[:computer_generated][i] += 1
   else
-    hash2[:computer_generated][i] = 1
+    second_hash[:computer_generated][i] = 1
   end
 end
 
-hash[:user_input].each do |i|
-  if hash2[:user_generated].key?(i)
-    hash2[:user_generated][i] += 1
+# fill second_hash with user_input values from first_hash
+first_hash[:user_input].each do |i|
+  if second_hash[:user_generated].key?(i)
+    second_hash[:user_generated][i] += 1
   else
-    hash2[:user_generated][i] = 1
+    second_hash[:user_generated][i] = 1
   end
 end
 
-puts hash
-puts hash2
+puts first_hash
+puts second_hash
 
 number = 0
 times = 0
 shows_text = ""
 generated_text = ""
 
+# ask user for number and provide information about how many times that number appears in each hash
 3.times do |i|
   print "What number do you want information about? "
   number = gets.chomp.to_i
   puts "Using only the first hash:"
-  hash.values.each do |array|
+  # loop through each array in first_hash
+  first_hash.values.each do |array|
+    # loop through each number in the array, and add 1 to times each time it appears
     array.each do |num|
       if num == number
         times += 1
       end
+      # check if number showed up
       if times > 0
         shows_text = "shows up #{times} time(s)"
       else
         shows_text = "did not show"
       end
-      if hash.values[0] == array
+      # check if array is the randomly or user generated one to determine what text to append
+      if first_hash.values[0] == array
         generated_text = "in the randomly generated numbers."
       else
         generated_text = "in the user generated numbers."
@@ -65,14 +77,17 @@ generated_text = ""
     times = 0
   end
   puts "Using only the second hash:"
-  hash2.values.each do |list|
+  # loop through each value in the second hash
+  second_hash.values.each do |list|
+    # if number exists in second_hash, assign key to times variable and update shows_text
     if list.has_key?(number)
       times = list[number]
       shows_text = "shows up #{times} time(s)"
     else
       shows_text = "did not show"
     end
-    if hash2.values[0] == list
+    # check if array is the randomly or user generated one to determine what text to append
+    if second_hash.values[0] == list
       generated_text = "in the randomly generated numbers."
     else
       generated_text = "in the user generated numbers."
