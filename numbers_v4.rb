30bd97a4abd1feadd739c85017439ae30ee87dc0
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-21 13:42:55 -0700

    refactor loops

--- numbers_v4.rb
+++ numbers_v4.rb
@@ -6,29 +6,27 @@ numbers = [0]
 number_count.times do |i|
   print "Enter a positive integer: "
   numbers.push(gets.chomp.to_i)
+  value = "equal to"
+
   if numbers[i + 1] < numbers[i]
-    puts "The value at index #{i}, #{numbers[i + 1]}, is less than the value at the last index, #{numbers[i]}."
+    value = "less than"
   elsif numbers[i + 1] > numbers[i]
-    puts "The value at index #{i}, #{numbers[i + 1]}, is greater than the value at the last index, #{numbers[i]}."
-  elsif numbers[i + 1] == numbers[i]
-    puts "The value at index #{i}, #{numbers[i + 1]}, is equal to the value at the last index, #{numbers[i]}."
+    value = "greater than"
   end
+
+  puts "The value at index #{i}, #{numbers[i + 1]}, is #{value} the value at the last index, #{numbers[i]}."
 end
 
 numbers.shift
 min = numbers[0]
 total = 0
 
-# loop to find lowest value in the numbers array
+# find total and lowest value
 numbers.each do |num|
+  total += num
   if num < min
     min = num
   end
-end
-
-# loop to find average of numbers array
-numbers.each do |num|
-  total += num
 end
 
 avg = (total.to_f / numbers.length).to_f
