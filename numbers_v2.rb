e3f34c2f17598f3c32f6e3245884a2b3d3be2846
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-21 13:25:57 -0700

    refactor to loop

--- numbers_v2.rb
+++ numbers_v2.rb
@@ -1,28 +1,13 @@
 # program that takes user input and then checks if the input is greater than or less than 20 and prints a string
-print "This program will ask you to enter numbers and share if the number is less than 20.
-Enter the 1st number: "
-user_input = gets.to_i
+puts "This program will ask you to enter numbers and share if the number is less than 20."
 
-if user_input >= 20
-  puts "#{user_input} is greater than or equal to 20."
-else
-  puts "#{user_input} is less than 20."
-end
+3.times do |i|
+  print "Enter number ##{i + 1}: "
+  user_input = gets.to_i
 
-print "Enter the 2nd number: "
-user_input2 = gets.to_i
-
-if user_input2 >= 20
-  puts "#{user_input2} is greater than or equal to 20."
-else
-  puts "#{user_input2} is less than 20."
-end
-
-print "Enter the 3rd number: "
-user_input3 = gets.to_i
-
-if user_input3 >= 20
-  puts "#{user_input3} is greater than or equal to 20."
-else
-  puts "#{user_input3} is less than 20."
+  if user_input >= 20
+    puts "#{user_input} is greater than or equal to 20."
+  else
+    puts "#{user_input} is less than 20."
+  end
 end
