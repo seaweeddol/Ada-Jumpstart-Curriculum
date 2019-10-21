2f809f3e512ac9d49fd927b360a05ac4150e5315
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-21 13:23:46 -0700

    refactor into loop

--- numbers.rb
+++ numbers.rb
@@ -1,13 +1,9 @@
 # program that takes user input of three numbers, and then prints them out with 20 added to each number in a string
-prompt = "enter three numbers"
-puts prompt
-user_input = gets.to_i
-user_input2 = gets.to_i
-user_input3 = gets.to_i
+numbers = []
 
-# add 20 to each user input
-user_input += 20
-user_input2 += 20
-user_input3 += 20
+3.times do |i|
+  puts "Enter ##{i}: "
+  numbers[i] = gets.chomp.to_i + 20
+end
 
-puts "Twenty added to the numbers you entered gives us #{user_input}, #{user_input2}, and #{user_input3}."
+puts "Twenty added to the numbers you entered gives us #{numbers[0]}, #{numbers[1]}, and #{numbers[2]}."
