3c861ce9dbb3c9471e34d7df960a6240df1871fa
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-21 13:23:58 -0700

    refactor to loop

--- madlibs.rb
+++ madlibs.rb
@@ -1,27 +1,47 @@
 # introduction to program
 puts "Welcome to MadLibs! Please enter the information below."
 
+madlibs = {
+  name: {
+    prompt: "name: ",
+  },
+  place: {
+    prompt: "word that starts with same letter as name: "
+  },
+  adjective: {
+    prompt: "adjective: ",
+  },
+  number: {
+    prompt: "number: ",
+  },
+  adjective2: {
+    prompt: "adjective: ",
+  },
+  animal: {
+    prompt: "animal: ",
+  },
+  adjective3: {
+    prompt: "another adjective: ",
+  },
+  noun: {
+    prompt: "noun: ",
+  },
+  verb: {
+    prompt: "verb ending in (ing): ",
+  },
+  noun2: {
+    prompt: "noun: ",
+  }
+}
+
+words = []
+
 # ask for user input, then save user input to variable, remove new line /n, and capitalize first letter
-print "name: "
-    name = gets.chomp.capitalize
-print "word that starts with same letter as name: "
-    place = gets.chomp.capitalize
-print "adjective: "
-    adjective = gets.chomp
-print "number: "
-    number = gets.chomp
-print "animal: "
-    animal = gets.chomp
-print "adjective: "
-    adjective2 = gets.chomp
-print "another adjective: "
-    adjective3 = gets.chomp
-print "noun: "
-    noun = gets.chomp
-print "verb ending in (ing): "
-    verb = gets.chomp
-print "noun: "
-    noun2 = gets.chomp
+madlibs.values.each do |intake|
+  print "#{intake[:prompt]}"
+  intake[:word] = gets.chomp
+  words.push(intake[:word])
+end
 
 # output mad lib with user input
-puts "Come to #{name}'s #{place}, where you can buy all sorts of #{adjective} things! You can get #{number} #{adjective2} #{animal}s, or one #{adjective3} #{animal}! You could buy a(n) #{noun} to use while #{verb}. The #{noun2} is the limit!"
+puts "Come to #{words[0]}'s #{words[1]}, where you can buy all sorts of #{words[2]} things! You can get #{words[3]} #{words[4]} #{words[5]}s, or one #{words[6]} #{words[5]}! You could buy a(n) #{words[7]} to use while #{words[8]}. The #{words[9]} is the limit!"
