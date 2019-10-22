09b42e9ca88e42c0b65f68368473aa3601379e4c
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-10-22 13:31:33 -0700

    refactor code duplication into methods

--- nim_game.rb
+++ nim_game.rb
@@ -1,7 +1,38 @@
 # Write a Ruby program that allows two users to play a game of Nim together.
 
 players = []
-piles = {}
+$piles = {}
+
+# create piles
+def create_piles(num)
+  num.times do |i|
+    $piles[:"#{(i + 65).chr}"] = rand(1..5)
+  end
+end
+
+def pile_check()
+  choice_valid = false
+  until choice_valid
+    pile_choice = gets.chomp.to_s.upcase
+    puts "#{pile_choice}: #{$piles[:"#{pile_choice}"]}"
+    if !$piles.key?(:"#{pile_choice}")
+      print "That pile does not exist. Choose again: "
+    elsif $piles[:"#{pile_choice}"] == 0
+      print "Pile #{pile_choice} is empty! Choose again: "
+    else
+      until choice_valid == true
+        print "How many sticks would you like to remove? "
+        amount = gets.to_i
+        if amount > $piles[:"#{pile_choice}"] || amount == 0
+          puts "You cannot remove #{amount} from pile."
+        else
+          $piles[:"#{pile_choice}"] -= amount
+          choice_valid = true
+        end
+      end
+    end
+  end
+end
 
 # ask user for name (player1)
 print "What is player 1's name? "
@@ -10,7 +41,7 @@ players.push(gets.chomp.capitalize)
 print "What is player 2's name? "
 players.push(gets.chomp.capitalize)
 
-# ask user for game difficulty (easy = 2 piles, medium = 3 piles, hard = 7 piles)
+# ask user for game difficulty
 print "Select your game difficulty
 Easy = 2 piles
 Medium = 3 piles
@@ -20,67 +51,33 @@ difficulty = gets.chomp.downcase
 
 # create piles hash based on difficult selected
 if difficulty == "easy"
-  2.times do |i|
-    piles[:"#{(i + 65).chr}"] = rand(1..5)
-  end
+  create_piles(2)
 elsif difficulty == "medium"
-  3.times do |i|
-    piles[:"#{(i + 65).chr}"] = rand(1..5)
-  end
+  create_piles(3)
 else
-  7.times do |i|
-    piles[:"#{(i + 65).chr}"] = rand(1..5)
-  end
+  create_piles(7)
 end
-
-puts piles
 
 # randomly decide first player (0..1)
 first_player = players[rand(0..1)]
 puts "The first player is #{first_player}"
 
-# game code
-# until all piles are empty
-until piles.all? {|a| a == 0}
-  turn = 0
-  if players[turn % 2] = first_player
-    print "Player 1, choose a pile: "
-    pile_choice = gets.chomp.to_s.upcase
-    puts pile_choice
-    puts piles[:"#{pile_choice}"]
-    if !piles.key?(:"#{pile_choice}")
-      puts "That pile does not exist."
-    elsif piles[:"#{pile_choice}"] == 0
-      puts "Pile #{pile_choice} is empty! Choose again"
-    else
-      print "How many sticks would you like to remove? "
-      amount = gets.to_i
-      if amount > piles[:"#{pile_choice}"] || amount == 0
-        puts "You cannot remove #{amount} from pile."
-      else
-        piles[:"#{pile_choice}"] -= amount
-      end
-    end
+turn = 0
+
+until $piles.values.all? {|a| a == 0}
+  puts $piles
+  if players[turn % 2] == first_player
+    print "#{players[0]}, choose a pile: "
+    pile_check()
+  else
+    print "#{players[1]}, choose a pile: "
+    pile_check()
   end
+  turn += 1
 end
-#       if pile_choice != available_pile
-#         print "that pile does not exist"
-#       elsif pile_choice_amount == 0
-#       else
-#         until amount is valid
-#           print "how many would you like to remove? "
-#           amount = gets.to_i
-#           if amount > pile_choice_amount OR amount == 0
-#             print "you cannot remove # from pile"
-#           end
-#         end
-#         pile_choice_amount -= amount
-#         push pile choice and pile choice amount to array
-#       end
-#     end
-#   else player 2
-#
+
 # after game finishes:
+puts "#{players[(turn - 1) % 2]} is the winner!"
 # print winner
 # print player #1 score
 # print player #1 move summary
