04652c5aa473029bc843439c399d69cffc5a39cb
Jessica <jessica.stone@concur.com>
2019-12-16 14:02:47 -0800

    clean up formatting

--- candymachine-v2.rb
+++ candymachine-v2.rb
@@ -2,6 +2,7 @@
 print "Welcome to The (Virtual) Vending Machine!\n
 How much money do you have? "
   $money = gets.chomp.to_f
+  puts "$#{'%.2f' % $money}? Here's what you can buy:"
 
 $menu = {
   A: {
@@ -26,34 +27,45 @@ $menu = {
   }
 }
 
-def print_menu()
-  puts "$#{'%.2f' % $money}? Here's what you can buy:"
-
+# print menu by looping through menu hash
+def display_menu()
+  print "\n"
   $menu.keys.each do |i|
     puts "  #{i}. $#{'%.2f' % $menu[:"#{i}"][:cost]} #{$menu[:"#{i}"][:selection]}"
   end
 
-  puts "Enter the letter of the item you want: "
+  puts "\nEnter the letter of the item you want: "
 end
 
 
-# check if user has enough money, and display options if so
+# check if user has enough money
 if $money < 0.5
   print "You can't afford anything here! Go somewhere else."
 else
-  while $money > 0.5
-    print_menu()
+  # while user has more than the cheapest option, accept user input
+  while $money >= 0.5
+    display_menu()
     $choice = gets.chomp.upcase
     choice_valid = false
+    # ask for user input until choice is a valid option
     until choice_valid == true
       if !$menu.key?(:"#{$choice}")
-        print "That's not an option, choose again! "
+        print "That's not an option, choose again: "
         $choice = gets.chomp.upcase
       else
-        choice_valid = true
-        $money -= $menu[:"#{$choice}"][:cost]
-        print "You bought #{$menu[:"#{$choice}"][:selection]}! You have $#{'%.2f' % $money} left."
+        if $money < $menu[:"#{$choice}"][:cost]
+          print "You can't afford that! Choose again: "
+          $choice = gets.chomp.upcase
+        else
+          choice_valid = true
+          # update money amount
+          $money -= $menu[:"#{$choice}"][:cost]
+          puts "You bought #{$menu[:"#{$choice}"][:selection]}! You have $#{'%.2f' % $money} left."
+        end
       end
     end
   end
 end
+
+puts "\nYou can't afford anything else.
+Thanks for shopping at The (Virtual) Vending Machine!"
